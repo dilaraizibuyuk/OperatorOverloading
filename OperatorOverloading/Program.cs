a4dc424f17df07f763ef3e01362d736c67decce9
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-14 18:34:29 +0300

    Proje dosyası ekle.

---
+++ OperatorOverloading/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sayim s1 = new Sayim(10);
+            Sayim s2 = new Sayim(10);
+            int s3 = s1 + s2;
+            Console.WriteLine(s3);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OperatorOverloading/Sayim.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    class Sayim
+    {
+        private int sayimiz;
+        public Sayim(int gelenSayi)
+        {
+            this.sayimiz = gelenSayi;
+        }
+        public void Yazdir()
+        {
+            Console.WriteLine(this.sayimiz);
+        }
+        public static int operator +(Sayim a,Sayim b)
+        {
+            return a.sayimiz + b.sayimiz;
+        }
+        public static implicit operator int(Sayim a)//operatör dönüştürme
+        {
+            return a.sayimiz;
+        }
+        public static implicit operator Sayim(int a)
+        {
+            return new Sayim(a);
+        }
+    }
+}
